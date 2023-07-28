2978e0999250aaed166846075a2203316c5c342d
patrickgorrell <5811734+patrickgorrell@users.noreply.github.com>
2023-07-27 23:37:28 -0700

    needed to add .env values for wallet addressses

--- scripts/deploy/deployGLUSD.ts
+++ scripts/deploy/deployGLUSD.ts
@@ -1,16 +1,26 @@
 import { ethers } from "hardhat";
 
 export const deployGLUSD = async () => {
-
     console.log(`deployGLUSD.ts Start: \n\n`);
 
+    // Define the addresses for admin, minter, and stakeModifier
+    const adminAddress = process.env.ADMIN_ADDRESS; // Replace with the actual address
+    const minterAddress = process.env.MINTER_ADDRESS; // Replace with the actual address
+    const stakeModifierAddress = process.env.STAKE_MODIFIER_ADDRESS; // Replace with the actual address
+
+    // Get the contract factory
     const contractFactory = await ethers.getContractFactory("GLUSD");
-  
-    const contract = await contractFactory.deploy();
-  
+
+    // Deploy the contract by passing the addresses as arguments
+    const contract = await contractFactory.deploy(
+        minterAddress,
+        adminAddress,
+        stakeModifierAddress
+    );
+
     await contract.deployed();
-  
+
     console.log(`deploy GLUSD deployed to: ${contract.address} \n\n`, `deployGLUSD.ts End: \n\n`);
 
     return contract.address;
-}
+};
--- scripts/deploy/deployGLX.ts
+++ scripts/deploy/deployGLX.ts
@@ -1,16 +1,26 @@
 import { ethers } from "hardhat";
 
 export const deployGLX = async () => {
-
     console.log(`deployGLX.ts Start: \n\n`);
 
+    // Define the addresses for admin, minter, and stakeModifier
+    const adminAddress = "0xYourAdminAddress"; // Replace with the actual address
+    const minterAddress = "0xYourMinterAddress"; // Replace with the actual address
+    const stakeModifierAddress = "0xYourStakeModifierAddress"; // Replace with the actual address
+
+    // Get the contract factory
     const contractFactory = await ethers.getContractFactory("GLX");
-  
-    const contract = await contractFactory.deploy();
-  
+
+    // Deploy the contract by passing the addresses as arguments
+    const contract = await contractFactory.deploy(
+        minterAddress,
+        adminAddress,
+        stakeModifierAddress
+    );
+
     await contract.deployed();
-  
+
     console.log(`deploy GLX deployed to: ${contract.address} \n\n`, `deployGLX.ts End: \n\n`);
 
     return contract.address;
-}
+};
